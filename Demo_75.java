e55133bc775a417b624bb694e4d8a90f70e4fe25
Selenium-Github <kr99.karishma@gmail.com>
2023-04-04 23:05:21 +0530

    first_commit

---
+++ Demo_75.java
@@ -0,0 +1,26 @@
+
+public class Demo_75 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int n= 221;
+	int rem;
+	
+	int rev=0;
+	
+	while(n>0)
+	{
+		rem=n%10;
+		rev=rev*10+rem;
+		n=n/10;
+		
+	}
+	System.out.println(rev);
+	
+	
+		
+
+	}
+
+}

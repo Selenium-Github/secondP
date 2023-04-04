52b7834a475419133e86377e0781c6bbdbc0c69b
Selenium-Github <kr99.karishma@gmail.com>
2023-04-04 23:11:42 +0530

    second_commit

---
+++ Demo_67.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class Demo_67 {
+
+	public static void main(String[] args) {
+		
+		int a= 20;
+		int b=30;
+		
+		int lar=a>b?a:b;
+		
+		System.out.println(lar);
+		// TODO Auto-generated method stub
+
+	}
+
+}
